bdbf7d600aec645910a5b2a3341ebb6f27498eb1
Micherlane Silva <micherlanerod@gmail.com>
2023-04-25 20:53:42 -0300

    Autenticação do usuário concluída.

--- AcervBooks/backend/src/routers.ts
+++ AcervBooks/backend/src/routers.ts
@@ -1,10 +1,15 @@
 import { Router} from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
+import { AuthUserController } from './controllers/user/AuthUserController';
 
 const router = Router();
 
 // ROTAS USER 
+
+// cadastro do usuário
 router.post('/users', new CreateUserController().handle);
+// autenticação do usuário
+router.post('/session', new AuthUserController().handle);
 
 export { router };
