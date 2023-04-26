3d9565953e767f2fa065ca196f5f529cf7979d48
Micherlane Silva <micherlanerod@gmail.com>
2023-04-26 19:36:01 -0300

    funcionalidade de detalhes do usuário.

--- AcervBooks/backend/src/routers.ts
+++ AcervBooks/backend/src/routers.ts
@@ -1,7 +1,12 @@
-import { Router} from 'express';
+import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController';
 import { AuthUserController } from './controllers/user/AuthUserController';
+import { DetailUserController } from './controllers/user/DetailUserController';
+
+import { CreateBookController } from './controllers/book/CreateBookController';
+
+import { isAuthenticated } from "./middlewares/isAutheticated";
 
 const router = Router();
 
@@ -9,7 +14,15 @@ const router = Router();
 
 // cadastro do usuário
 router.post('/users', new CreateUserController().handle);
-// autenticação do usuário
+// login do usuário
 router.post('/session', new AuthUserController().handle);
+// detalhes do usuário - perfil
+router.get('/me', isAuthenticated, new DetailUserController().handle);
+
+
+// ROTAS BOOK
+
+//cadastro de livro
+router.post('/books', isAuthenticated, new CreateBookController().handle);
 
 export { router };
---
+++ AcervBooks/backend/src/services/user/DetailUserService.ts
@@ -0,0 +1,22 @@
+import prismaClient from "../../prisma";
+
+interface DetailRequest {
+    user_id: string;
+}
+
+class DetailUserService {
+    async execute({ user_id }: DetailRequest) {
+        const user = await prismaClient.user.findMany({
+            where:{
+                id: user_id
+            },
+            include: {
+                booksSaved: true,
+            }
+        });
+
+        return user;
+    }
+}
+
+export { DetailUserService }
